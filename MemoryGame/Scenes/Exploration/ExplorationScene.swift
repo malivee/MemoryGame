1d05ddd248f1a14f4cfaf12b28bc1b83d84344b0
malivee <muhalif7123@gmail.com>
2026-09-18 09:03:05 +0700

    horizontal deck

--- MemoryGame/Scenes/Exploration/ExplorationScene.swift
+++ MemoryGame/Scenes/Exploration/ExplorationScene.swift
@@ -1026,7 +1026,7 @@ final class ExplorationScene: SKScene {
             say("Arthur masih diperhatikan. Berlindung sampai warga tenang."); return
         }
         PrologueStore.shared.save()
-        let photo = DeckPuzzleScene(size: size)
+        let photo = RightDeckPuzzleScene(size: size)
         photo.bagSelectedPiece = selectedPiece
         photo.scaleMode = .resizeFill
         view?.presentScene(photo, transition: .fade(withDuration: 0.35))
